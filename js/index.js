a837584b3bd180dab2c032399bdabe6d9ce875f3
annyonok <annyonok@gmail.com>
2024-09-03 20:48:51 +0300

    add background and burger

--- js/index.js
+++ js/index.js
@@ -1,38 +1,44 @@
-new Swiper('.swiper', {
-    navigation: {
-        nextEl: '.portfolio__arrow_right',
-        prevEl: '.portfolio__arrow_left',
-        disabledClass: 'portfolio__arrow_disable',
-      },
-      // Responsive breakpoints
-    breakpoints: {
-    // when window width is >= 480px
-    480: {
-      grid: {
-        fill: 'row',
-        rows: 1
-      }
-    },
-    // when window width is >= 768px
-    768: {
-        slidesPerView: 2,
-        grid: {
-            fill: 'row',
-            rows: 2
-          }        
-        },
-    // when window width is >= 1440px
-    1440: {
-        slidesPerView: 2,
-        grid: {
-            fill: 'row',
-            rows: 2
-          }        
-        }
-  }
-});
-
 // Menu
-$('.header__contacts-burger').click(() => {
-    $('.header__contacts').slideToggle().css('display', 'flex');
-})
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
+
+open.addEventListener('click', () => container.classList.add('show-nav'))
+
+close.addEventListener('click', () => container.classList.remove('show-nav'))
+
+
+
+// new Swiper('.swiper', {
+//   navigation: {
+//       nextEl: '.portfolio__arrow_right',
+//       prevEl: '.portfolio__arrow_left',
+//       disabledClass: 'portfolio__arrow_disable',
+//     },
+//     // Responsive breakpoints
+//   breakpoints: {
+//   // when window width is >= 480px
+//   480: {
+//     grid: {
+//       fill: 'row',
+//       rows: 1
+//     }
+//   },
+//   // when window width is >= 768px
+//   768: {
+//       slidesPerView: 2,
+//       grid: {
+//           fill: 'row',
+//           rows: 2
+//         }        
+//       },
+//   // when window width is >= 1440px
+//   1440: {
+//       slidesPerView: 2,
+//       grid: {
+//           fill: 'row',
+//           rows: 2
+//         }        
+//       }
+// }
+// });
