78430709ee8a20b53e78b89dfebb5d1bd1c076e7
annyonok <annyonok@gmail.com>
2024-09-16 09:03:29 +0300

    added photo

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+const aboutPhoto = document.querySelector('.about__photo')
+
 // Menu
 const burgerOpen = document.querySelector('.header__contacts-burger');
 const burgerClose = document.querySelector('.header__contacts-burger-close');
@@ -22,6 +24,27 @@ function menuShow() {
 }
 
 menuShow();
+
+// Двигаем фотку
+aboutPhoto.addEventListener('click', (event) => {
+    const rect = aboutPhoto.getBoundingClientRect()
+
+    const offfsetX = event.clientX - rect.left - rect.width / 2
+    const offfsetY = event.clientY - rect.top - rect.height / 2
+
+    const DEG = 50
+
+    const tiltX = (offfsetY / rect.height) * DEG
+    const tiltY = (offfsetX / rect.width) * -DEG
+
+    aboutPhoto.style.setProperty('--tiltX', `${tiltX}deg`)
+    aboutPhoto.style.setProperty('--tiltY', `${tiltY}deg`)
+
+    setTimeout(() => {
+        aboutPhoto.style.setProperty('--tiltX', `0deg`)
+        aboutPhoto.style.setProperty('--tiltY', `0deg`)
+    }, 300)
+})
 
 
 
